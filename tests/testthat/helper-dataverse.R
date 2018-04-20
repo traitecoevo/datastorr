0af2f0ccb40e5efbab1f4bb6631c1f5d2cd8ae2b
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-04-20 10:07:34 +0100

    Fix whitespace

--- tests/testthat/helper-dataverse.R
+++ tests/testthat/helper-dataverse.R
@@ -5,12 +5,14 @@ skip_if_no_downloads <- function() {
   }
   skip("Skipping downloads")
 }
+
 skip_unless_internet <- function() {
   if (has_internet()) {
     return()
   }
   skip("No internet :(")
 }
+
 skip_if_no_github_token <- function() {
   skip_if_no_downloads()
   if (inherits(github_token(), "request")) {
@@ -18,13 +20,14 @@ skip_if_no_github_token <- function() {
   }
   skip("No GITHUB_TOKEN set")
 }
+
 has_internet <- function() {
   !is.null(suppressWarnings(nsl("www.google.com")))
 }
 
 ## I don't think that this wants to be part of the main bit of the
 ## package as it's a bit savage but it'll do for now:
-github_api_delete_all_releases <- function(info, yes=!interactive()) {
+github_api_delete_all_releases <- function(info, yes = !interactive()) {
   d <- github_api_releases(info)
   for (x in d) {
     github_api_release_delete(info, I(x$tag_name), yes)
